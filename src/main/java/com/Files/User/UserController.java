87afaf72cd286b4fd5dc9ddfa1aef0e92014aa22
Tailor Shaikshavali <138803242+TechShaik@users.noreply.github.com>
2024-11-13 10:20:22 +0530

    Update UserController.java

--- src/main/java/com/Files/User/UserController.java
+++ src/main/java/com/Files/User/UserController.java
@@ -26,7 +26,7 @@ import com.Files.User.DTOs.UserResponseDTO;
 	
 	@GetMapping
 	public String UW() {
-		return "Welcome User Please Register First";
+		return "Welcome User Register First";
 	}
 	@PostMapping("/reg")
 	public UserResponseDTO regUSer(@RequestBody UserRequsetDTO userRequsetDTO) {
